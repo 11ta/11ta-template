f7d29ce211c11de48d423be5af9f248d68774364
Shane Robinson <shane@barefeetstudios.com>
2020-08-18 17:18:08 -0600

    dumped Tailwind UI & Typography, + Custom Forms

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,21 +1,8 @@
-const defaultTheme = require("tailwindcss/defaultTheme");
-
 module.exports = {
-  purge: [
-    './src/**/*.html',
-    './src/**/*.njk',
-    './src/**/*.md'
-  ],
-  theme: {
-    extend: {
-      fontFamily: {
-        sans: ["Inter var", ...defaultTheme.fontFamily.sans],
-      },
-    },
-  },
-  variants: {},
-  plugins: [
-    require('@tailwindcss/ui'),
-    require('@tailwindcss/typography'),
-  ],
+	purge: ['./src/**/*.html', './src/**/*.njk', './src/**/*.md'],
+	theme: {
+		extend: {},
+	},
+	variants: {},
+	plugins: [require('@tailwindcss/custom-forms')],
 }
