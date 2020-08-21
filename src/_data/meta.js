946fc9050e632511132e409ca155ad576bdc42f1
Shane Robinson <shane@barefeetstudios.com>
2020-08-21 15:15:25 -0600

    added RSS feed

---
+++ src/_data/meta.js
@@ -0,0 +1,10 @@
+module.exports = {
+	siteURL: process.env.URL || 'http://localhost:8080',
+	siteName: "Shane Robinson's 11ty | TailwindCSS | Alpine.js Template",
+	siteDescription:
+		'A highly configurable 11ty template to quickly spin up a fully functional website with TailwindCSS and Alpine.js baked in.',
+	lang: 'en',
+	locale: 'en_us',
+	authorName: 'Shane Robinson',
+	authorEmail: 'shane@shanerobinson.com',
+}
--- utils/filters.js
+++ utils/filters.js
@@ -1,71 +1,60 @@
 const { DateTime } = require('luxon')
-const slugify = require("slugify");
-
+const slugify = require('slugify')
 
 module.exports = {
+	/**
+	 * Filters
+	 * @link https://www.11ty.dev/docs/filters/
+	 */
 
-  /**
-   * Filters
-   * @link https://www.11ty.io/docs/filters/
-   */
+	/**
+	 * dateToFormat allows specifiying display format at point of use.
+	 * Example in footer: {{ build.timestamp | dateToFormat('yyyy') }}
+	 * uses .timestamp from the _data/build.js export and formats it via dateToFormat below
+	 */
+	dateToFormat: (date, format) => {
+		return DateTime.fromJSDate(date, {
+			zone: 'utc',
+		}).toFormat(String(format))
+	},
 
-   /**
-    * dateToFormat allows specifiying display format at point of use.
-    * Example in footer: {{ build.timestamp | dateToFormat('yyyy') }}
-    * uses .timestamp from the _data/build.js export and formats it via dateToFormat below
-    */
-  dateToFormat: (date, format) => {
-    return DateTime.fromJSDate(date, { 
-      zone: 'utc'
-    }).toFormat(String(format))
-  },
+	htmlDateString: (dateObj) => {
+		return DateTime.fromJSDate(dateObj, {
+			zone: 'utc',
+		}).toFormat('yyyy-LL-dd')
+	},
 
-  htmlDateString: (dateObj) => {
-    return DateTime.fromJSDate(dateObj, {
-      zone: 'utc'
-    }).toFormat('yyyy-LL-dd');
-  },
+	machineDate: (dateObj) => {
+		return DateTime.fromJSDate(dateObj, {
+			zone: 'utc',
+		}).toFormat('yyyy-MM-dd')
+	},
 
-  machineDate: (dateObj) => {
-    return DateTime.fromJSDate(dateObj, {
-      zone: 'utc'
-    }).toFormat('yyyy-MM-dd');
-  },
+	readableDate: (dateObj) => {
+		return DateTime.fromJSDate(dateObj, {
+			zone: 'utc',
+		}).toFormat('dd LLL yyyy')
+	},
 
-  readableDate: (dateObj) => {
-    return DateTime.fromJSDate(dateObj, {
-      zone: 'utc'
-    }).toFormat('dd LLL yyyy');
-  },
+	/**
+	 * Date for Permalinks when working with old /YYYY/MM/DD/slug format from Wordpress exports
+	 * Used in building Permalinks in /src/posts/posts.json
+	 * @link /src/posts/posts.json
+	 */
+	dateToPermalink: (dateObj) => {
+		return DateTime.fromJSDate(dateObj, {
+			zone: 'utc',
+		}).toFormat('yyyy/MM/dd')
+	},
 
-
-
-
-
-
-
-  /**
-   * Date for Permalinks when working with old /YYYY/MM/DD/slug format from Wordpress exports
-   * @link https://wp11.netlify.app
-   * Used in building Permalinks in /src/posts/posts.json
-   * @link /src/posts/posts.json
-   */
-  dateToPermalink: (dateObj) => {
-    return DateTime.fromJSDate(dateObj, {
-      zone: 'utc'
-    }).toFormat('yyyy/MM/dd');
-  },
-
-  /**
+	/**
    // Universal slug filter strips unsafe chars from URLs
    */
-  slugify: (string) => {
-    return slugify(string, {
-      lower: true,
-      replacement: "-",
-      remove: /[*+~.·,()'"`´%!?¿:@]/g
-    });
-  },
-
-
+	slugify: (string) => {
+		return slugify(string, {
+			lower: true,
+			replacement: '-',
+			remove: /[*+~.·,()'"`´%!?¿:@]/g,
+		})
+	},
 }
