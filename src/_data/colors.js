e79f0aaba12e8d31d6d8bf0ec30de30619b9c61d
Shane Robinson <shane@barefeetstudios.com>
2020-09-18 10:00:58 -0600

    tailwind post and package upates

--- src/_data/colors.js
+++ src/_data/colors.js
@@ -1,5 +1,13 @@
 module.exports = {
 	/**
+	 * ************************ IMPORTANT *********************************
+	 *  A few colors for links, blockquotes, code, etc. are set in the
+	 *  /src/assets/css/tailwind.css file.
+	 *  So if you can't find what you're looking for below,
+	 *  check out that tailwind.css file.
+	 * ********************************************************************
+	 *
+	 *
 	 * ===== Global Site Text Color =====
 	 */
 	siteTextDefault: 'text-gray-900',
